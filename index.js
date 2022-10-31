d17b9680b8a7cb7d468907b707edb4953ae3cb3f
Gui <gsardaparreu@gmail.com>
2022-10-31 20:37:56 +0100

    feat: add hover animation

--- index.js
+++ index.js
@@ -4,10 +4,25 @@ $(document).ready(() => {
   $imgPckr.on('click', () => $imgInput.trigger('click'));
   $imgPckr.hover(
     () => {
-      $('#button-title').removeClass('hidden');
+      /* Since the parameter 'now' from the step function is inaccurate we have created 
+      this 'animationStep' counter that will force the slideDown method to be called only once. */
+      let animationStep = 0;
+      $('#upload-container').animate(
+        { height: 'show' },
+        {
+          duration: 300,
+          step: () => {
+            animationStep == 10 && $('#upload-header').slideDown('fast');
+            animationStep++;
+          },
+        }
+      );
     },
     () => {
-      $('#button-title').addClass('hidden');
+      $('#upload-header').hide();
+      $('#upload-container').stop(true).animate({ height: 'hide' }); // stop removes all the animations on the given element
     }
   );
 });
+
+// bg-yellow-200/40
