4cb9b0bd9bcd29199669283de60dde9c3b902f6c
Gui <gsardaparreu@gmail.com>
2022-11-01 20:43:08 +0100

    feat: implement image pick

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 $(document).ready(() => {
   const $imgPckr = $('#image-picker');
   const $imgInput = $('#image-input');
+
+  /* EVENT BINDERS*/
   $imgPckr.on('click', () => $imgInput.trigger('click'));
+
+  // Source of the followinf function: https://stackoverflow.com/a/12368976
+  $imgInput.on('change', (e) => {
+    const file = e.originalEvent.srcElement.files[0];
+    const reader = new FileReader();
+
+    reader.onloadend = function () {
+      $imgPckr.css('background-image', 'url(' + reader.result + ')');
+    };
+
+    reader.readAsDataURL(file);
+  });
+
   $imgPckr.hover(
     () => {
       /* Since the parameter 'now' from the step function is inaccurate we have created 
-      this 'animationStep' counter that will force the slideDown method to be called only once. */
+      this 'animationStep' counter that will force the 'slideDown' method to be called only once. */
       let animationStep = 0;
       $('#upload-container').animate(
         { height: 'show' },
@@ -20,9 +35,8 @@ $(document).ready(() => {
     },
     () => {
       $('#upload-header').hide();
-      $('#upload-container').stop(true).animate({ height: 'hide' }); // stop removes all the animations on the given element
+      // The 'stop' method removes all the animations on the given element.
+      $('#upload-container').stop(true).animate({ height: 'hide' });
     }
   );
 });
-
-// bg-yellow-200/40
